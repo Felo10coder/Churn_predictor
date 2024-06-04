4b0fbde642dd992fe05beca66f0fd61d1d18b4cd
Felix Kwemoi <felixkwemoi7@gmail.com>
2024-06-04 16:36:29 +0300

    selecting what data to display

---
+++ pages/03_🚀_Predict.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "Prediction page",
+    page_icon = "🚀",
+    layout = 'wide'
+)
+st.title('CUSTOMER CHURN PREDICTION')
+st.sidebar.success("select a page above")
---
+++ pages/04_📜_History.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "History page",
+    page_icon = "📜",
+    layout = 'wide'
+)
+st.title('CUSTOMER CHURN HISTORY')
+st.sidebar.success("select a page above")
